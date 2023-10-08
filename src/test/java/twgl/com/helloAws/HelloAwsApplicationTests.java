de7951fd766709632ff24e532d0e9133d2f8c6ec
Tadeu Wohlers <tadeu.wohlers@gmail.com>
2023-10-07 22:42:27 -0300

    project setup

---
+++ src/test/java/twgl/com/helloAws/HelloAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package twgl.com.helloAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
